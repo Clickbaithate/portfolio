8f7bd7f629fa4d6b4f2ce697441c7768221159c4
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-29 16:04:56 -0500

    Fixing Casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Project from './pages/project';
+import Project from './pages/Project';
 import ScrollToTop from './components/scroll_to_top';
 
 const App = () => {
