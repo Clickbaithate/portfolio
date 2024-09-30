e0cdfd6436910a406a6ecad6602fcd5cf146bba1
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-30 01:17:18 -0500

    i dont even know anymore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Navbar from '../components/Navbar';
 import Carousel from '../components/Carousel';
 import Timeline from '../components/Timeline';
-import { Link } from 'react-router-dom';
 import MyButton from '../components/MyButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  
   const roles = ["Web Developer", "Mobile Developer", "Software Engineer"];
   const [displayedText, setDisplayedText] = useState("");
   const [roleIndex, setRoleIndex] = useState(0);
@@ -16,9 +15,12 @@ const Home = () => {
   const [isErasing, setIsErasing] = useState(false);
   const [speed, setSpeed] = useState(100);
   const [isPaused, setIsPaused] = useState(false);
+  
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
 
   useEffect(() => {
-    
     if (isPaused) return;
 
     const timeout = setTimeout(() => {
@@ -48,87 +50,89 @@ const Home = () => {
     return () => clearTimeout(timeout);
   }, [displayedText, isErasing, charIndex, roleIndex, roles, isPaused, speed]);
 
+  const scrollToSection = (ref) => {
+    if (ref.current) {
+      const targetPosition = ref.current.getBoundingClientRect().top + window.scrollY;
+      const offset = -100;
+      const scrollToPosition = targetPosition + offset;
+
+      window.scrollTo({
+        top: scrollToPosition,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
-    <div className="bg-[url('assets/background.svg')] bg-cover min-h-screen">
-      <header className="">
-      <nav className="container pt-5 pb-5 ml-[100px]">
-        <div className="flex items-center justify-between">
-          <div className="text-white font-bold text-xl">
-            <Link to="/" className="flex items-center">
-              <div className="w-20 h-20 bg-[url('assets/logo.svg')] bg-cover rounded-full" />
-            </Link>
-          </div>
-          
-          <div className="flex items-center space-x-16">
-            <div className="">
-              <ul className="flex items-center space-x-8">
-                <li>
-                  <Link to="#about" className="text-white font-bold">
-                    <MyButton>About</MyButton>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#skills" className="text-white font-bold ">
-                    <MyButton>Skills</MyButton>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#projects" className="text-white font-bold ">
-                    <MyButton>Projects</MyButton>
-                  </Link>
-                </li>
-              </ul>
+    <div ref={aboutRef} className="bg-[url('assets/background.svg')] bg-cover min-h-screen">
+      <header className="bg-[#1a161d] fixed top-0 left-0 right-0 z-50">
+        <nav className="container pt-1 pb-1 ml-[100px]">
+          <div className="flex items-center justify-between">
+            <div className="text-white font-bold text-xl">
+              <Link to="/" className="flex items-center">
+                <div className="w-20 h-20 bg-[url('assets/logo.svg')] bg-cover rounded-full" />
+              </Link>
             </div>
-
-            <div className="hidden md:flex items-center space-x-8 list-none">
-              <li>
-                <Link to="https://www.linkedin.com/in/gaelgg/" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon className="text-white rounded-full w-8 h-8 transition-all ease-in-out duration-500 transform hover:scale-150" icon={faLinkedin} />
-                </Link>
-              </li>
-              <li>
-                <Link to="https://github.com/Clickbaithate" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon className="text-white rounded-full w-8 h-8 transition-all ease-in-out duration-500 transform hover:scale-150" icon={faGithub} />
-                </Link>
-              </li>
-              <li>
-                <Link to="mailto:gaelguzman.dev@gmail.com" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon className="text-white rounded-full w-8 h-8 transition-all ease-in-out duration-500 transform hover:scale-150" icon={faGoogle} />
-                </Link>
-              </li>
+            <div className="flex items-center space-x-16">
+              <div>
+                <ul className="flex items-center space-x-8">
+                  <li>
+                    <button onClick={() => scrollToSection(aboutRef)} className="text-white font-bold">
+                      <MyButton>About</MyButton>
+                    </button>
+                  </li>
+                  <li>
+                    <button onClick={() => scrollToSection(skillsRef)} className="text-white font-bold">
+                      <MyButton>Skills</MyButton>
+                    </button>
+                  </li>
+                  <li>
+                    <button onClick={() => scrollToSection(projectsRef)} className="text-white font-bold">
+                      <MyButton>Projects</MyButton>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              <div className="hidden md:flex items-center space-x-8 list-none">
+                <li>
+                  <Link to="https://www.linkedin.com/in/gaelgg/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="text-white rounded-full w-8 h-8 transition-all ease-in-out duration-500 transform hover:scale-150" icon={faLinkedin} />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="https://github.com/Clickbaithate" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="text-white rounded-full w-8 h-8 transition-all ease-in-out duration-500 transform hover:scale-150" icon={faGithub} />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="mailto:gaelguzman.dev@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon className="text-white rounded-full w-8 h-8 transition-all ease-in-out duration-500 transform hover:scale-150" icon={faGoogle} />
+                  </Link>
+                </li>
+              </div>
+            </div>
+            <div className="md:hidden">
+              <button className="outline-none mobile-menu-button">
+                <svg
+                  className="w-6 h-6 text-white"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+              </button>
             </div>
           </div>
+        </nav>
+      </header>
 
-          <div className="md:hidden">
-            <button className="outline-none mobile-menu-button">
-              <svg
-                className="w-6 h-6 text-white"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-
-        <div className="mobile-menu hidden md:hidden">
-          <ul className="mt-4 space-y-4">
-            <li><Link to="/" className="block px-4 py-2 text-white bg-gray-900 rounded">Home</Link></li>
-            <li><Link to="/about" className="block px-4 py-2 text-white bg-gray-900 rounded">About</Link></li>
-            <li><Link to="/services" className="block px-4 py-2 text-white bg-gray-900 rounded">Services</Link></li>
-            <li><Link to="/contact" className="block px-4 py-2 text-white bg-gray-900 rounded">Contact</Link></li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-      <div className="w-full h-screen flex items-center" style={{ height: 'calc(100vh - 200px)' }}>
+      <div className="mt-[88px] w-full h-screen flex items-center" style={{ height: 'calc(100vh - 200px)' }}>
         <div className="w-full md:w-1/2 p-4">
-          <div id="about" className="text-white text-5xl font-bold ml-[100px] md:text-5xl text-xl">
+          <div className="text-white text-5xl font-bold ml-[100px] md:text-5xl text-xl">
             Hi! I'm Gael!
           </div>
           <div className="text-white text-5xl font-bold ml-[100px] py-4 md:text-5xl text-xl">
@@ -155,14 +159,14 @@ const Home = () => {
           <div className="animate-moveUpDown bg-[url('assets/person.svg')] bg-cover w-[600px] h-[600px]"></div>
         </div>
       </div>
-      <h1 id="projects" className="text-white text-5xl font-bold ml-[100px] md:text-5xl text-xl">Personal Projects</h1>
-      <div className="">
-        <div className="h-[700px] flex justify-center items-center">
-          <Carousel/>
-        </div>
+
+      <h1 ref={projectsRef} className="text-white text-5xl font-bold ml-[100px] md:text-5xl text-xl">Personal Projects</h1>
+      <div className="h-[700px] flex justify-center items-center">
+        <Carousel />
       </div>
-      <h1 id="skills" className="text-white text-5xl font-bold ml-[100px] md:text-5xl text-xl mb-5">Skills</h1>
-      <Timeline className="" ></Timeline>
+
+      <h1 ref={skillsRef} className="text-white text-5xl font-bold ml-[100px] md:text-5xl text-xl mb-5">Skills</h1>
+      <Timeline />
     </div>
   );
 };
